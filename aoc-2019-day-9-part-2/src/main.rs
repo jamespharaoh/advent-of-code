6cc0658b1c6452d02de6c3f3d94aaa5484d280d0
James Pharaoh <james@pharaoh.uk>
2021-04-27 22:20:27 +0200

    day 9

---
+++ aoc-2019-day-9-part-2/src/main.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+mod intcode;
+
+fn main () {
+	let programme_source = fs::read_to_string ("input").unwrap ();
+	let programme = intcode::from_str (& programme_source);
+	let mut machine = intcode::Machine::new (programme);
+	machine.queue_input (2);
+	loop {
+		match machine.run () {
+			intcode::RunResult::Output (value) => println! ("Output: {}", value),
+			intcode::RunResult::Halt => break,
+		}
+	}
+}
