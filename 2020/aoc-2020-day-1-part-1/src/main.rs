ec48e48b23b4b5c2859e83a4992a9d758969f454
James Pharaoh <james@pharaoh.uk>
2021-05-02 13:49:23 +0200

    year 2020 day 1

---
+++ 2020/aoc-2020-day-1-part-1/src/main.rs
@@ -0,0 +1,20 @@
+use std::fs;
+
+fn main () {
+	let input_string = fs::read_to_string ("input").unwrap ();
+	let input_lines: Vec <& str> = input_string.split ('\n').collect ();
+	let input_values: Vec <u64> = input_lines.into_iter ().filter (
+		|line| line.len () > 0,
+	).map (
+		|line| line.parse ().unwrap (),
+	).collect ();
+	for x in 0 .. input_values.len () - 1 {
+		for y in x + 1 .. input_values.len () {
+			if input_values [x] + input_values [y] == 2020 {
+				println! ("First input value: {}", input_values [x]);
+				println! ("Second input value: {}", input_values [y]);
+				println! ("Product of both values: {}", input_values [x] * input_values [y]);
+			}
+		}
+	}
+}
