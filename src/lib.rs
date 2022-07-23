2d1a1e591d0b0e577a6cf96daf41f7a1b915b63c
James Pharaoh <james@pharaoh.uk>
2022-07-23 16:13:01 +0200

    github: add coverage from tarpaulin to published docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,9 @@
+//! Advent of Code solutions by James Pharaoh
+//!
+//! [https://adventofcode.com/](https://adventofcode.com/)
+//!
+//! Home: [https://github.com/jamespharaoh/advent-of-code](https://github.com/jamespharaoh/advent-of-code)  
+
 pub use aoc_2015 as year_2015;
 pub use aoc_2021 as year_2021;
+pub use aoc_common as common;
