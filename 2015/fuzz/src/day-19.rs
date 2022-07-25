dc70cfa66744c25c8b8e6611c30a4b138e5956c1
James Pharaoh <james@pharaoh.uk>
2022-07-25 13:57:14 +0200

    2015/day-19: fix fuzzer

--- 2015/fuzz/src/day-19.rs
+++ 2015/fuzz/src/day-19.rs
@@ -5,7 +5,7 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target! (|input_str: & str| {
 	let input_vec = input_str.split ('\n').collect::<Vec <_>> ();
-	if let Ok (input) = model::parse_input (& input_vec) {
+	if let Ok (input) = model::Input::parse (& input_vec) {
 		let _ = logic::part_one (input.clone ());
 		let _ = logic::part_two (input.clone ());
 	}
