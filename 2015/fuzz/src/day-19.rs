60809cf6f2524cfe8c1d163291dcdc29f9c1a330
James Pharaoh <james@pharaoh.uk>
2022-07-25 14:41:13 +0200

    2015/day-19: limit fuzzing to short molecules to prevent pathologically slow execs

--- 2015/fuzz/src/day-19.rs
+++ 2015/fuzz/src/day-19.rs
@@ -5,6 +5,7 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target! (|input_str: & str| {
 	let input_vec = input_str.split ('\n').collect::<Vec <_>> ();
+	if input_vec.last ().unwrap ().len () > 16 { return }
 	if let Ok (input) = model::Input::parse (& input_vec) {
 		let _ = logic::part_one (input.clone ());
 		let _ = logic::part_two (input.clone ());
