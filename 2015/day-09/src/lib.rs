5ed8af7381fb27cf3aa53b116a29692d77cbee40
James Pharaoh <james@pharaoh.uk>
2022-07-28 09:13:04 +0200

    2015/day-09: add fuzzing

--- 2015/day-09/src/lib.rs
+++ 2015/day-09/src/lib.rs
@@ -1,6 +1,12 @@
 //! Advent of Code 2015: Day 9: All in a Single Night
 //!
 //! [https://adventofcode.com/2015/day/9](https://adventofcode.com/2015/day/9)
+//!
+//! # Input
+//!
+//! Each line represents the distance between two locations in the form "$0 to $1 = $2". The first
+//! two parameters are alphanumeric strings representing the locations, tand the third is a
+//! integer representing the distance.
 
 use aoc_common::*;
 
@@ -8,34 +14,54 @@ puzzle_info! {
 	name = "All in a Single Night";
 	year = 2015;
 	day = 9;
+	parse = |input| model::parse_input (input);
 	part_one = |input| logic::part_one (input);
 	part_two = |input| logic::part_two (input);
 }
 
-mod logic {
+pub mod logic {
 
 	use super::*;
 	use model::Input;
 	use model::Place;
 
-	pub fn part_one (input: & [& str]) -> GenResult <u32> {
-		let input = model::parse_input (input) ?;
+	pub fn part_one (input: Input) -> GenResult <u32> {
+		sanity_check (& input) ?;
 		Ok (distances (& input).min ().unwrap ())
 	}
 
-	pub fn part_two (input: & [& str]) -> GenResult <u32> {
-		let input = model::parse_input (input) ?;
+	pub fn part_two (input: Input) -> GenResult <u32> {
+		sanity_check (& input) ?;
 		Ok (distances (& input).max ().unwrap ())
 	}
 
-	pub fn distances (input: & Input) -> impl Iterator <Item = u32> {
+	fn sanity_check (input: & Input) -> GenResult <()> {
+		if input.len () > 60 { Err ("Refusing to handle more than 60 distances") ?; }
+		let num_places =
+			input.iter ()
+				.flat_map (|(from, to, _)| [from, to])
+				.sorted ()
+				.dedup ()
+				.count ();
+		if num_places > 10 {
+			Err ("Refusing to handle more than 10 places") ?;
+		}
+		if input.len () != num_places * (num_places - 1) / 2 {
+			Err ("Wrong number of distances for given number of places") ?;
+		}
+		Ok (())
+	}
+
+	fn distances (input: & Input) -> impl Iterator <Item = u32> {
 		let (num_places, distances) = gen_dist_table (input);
 		let mut route = Vec::new ();
 		let mut used = iter::repeat (false).take (num_places).collect::<Vec <_>> ();
 		let mut finished = false;
 		iter::from_fn (move || {
 			if finished { return None }
+
 			// find the next free place
+
 			#[ inline ]
 			fn next (used: & [bool], start: usize) -> Option <usize> {
 				used.iter ().copied ().enumerate ()
@@ -44,7 +70,9 @@ mod logic {
 					.map (|(idx, _)| idx)
 					.next ()
 			}
+
 			// find a place index we can increment (except first time)
+
 			if ! route.is_empty () {
 				loop {
 					if let Some (val) = route.pop () {
@@ -57,19 +85,24 @@ mod logic {
 					} else { finished = true; return None }
 				}
 			}
+
 			// fill in the rest with the minimum possible
+
 			while route.len () < num_places {
 				let val = next (& used, 0).unwrap ();
 				route.push (val);
 				used [val] = true;
 			}
+
 			// return
+
 			Some (
 				route.iter ()
 					.tuple_windows::<(_, _)> ()
 					.map (|(place_0, place_1)| distances [place_0 * num_places + place_1])
 					.sum::<u32> ()
 			)
+
 		})
 	}
 
@@ -95,7 +128,7 @@ mod logic {
 
 }
 
-mod model {
+pub mod model {
 
 	use super::*;
 
@@ -110,6 +143,7 @@ mod model {
 				let place_0 = parser.word () ?;
 				let place_1 = parser.expect_word ("to") ?.word () ?;
 				let distance = parser.expect_word ("=") ?.int () ?;
+				if distance < 1 { Err ("Distance must be at least one") ?; }
 				parser.end () ?;
 				Ok ((place_0.into (), place_1.into (), distance))
 			}).map_parse_err (|col_idx|
@@ -132,15 +166,15 @@ mod examples {
 	];
 
 	#[ test ]
-	fn part_one () -> GenResult <()> {
-		assert_eq! (605, logic::part_one (EXAMPLE) ?);
-		Ok (())
+	fn part_one () {
+		let puzzle = puzzle_metadata ();
+		assert_eq_ok! ("605", puzzle.part_one (EXAMPLE));
 	}
 
 	#[ test ]
-	fn part_two () -> GenResult <()> {
-		assert_eq! (982, logic::part_two (EXAMPLE) ?);
-		Ok (())
+	fn part_two () {
+		let puzzle = puzzle_metadata ();
+		assert_eq_ok! ("982", puzzle.part_two (EXAMPLE));
 	}
 
 }
