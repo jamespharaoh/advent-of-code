16a647c8e6c049e1743c7058856391b96e9f32a0
James Pharaoh <james@pharaoh.uk>
2022-08-03 17:43:10 +0000

    2015/day-04: much faster test cases, and command to find them

--- 2015/day-04/src/lib.rs
+++ 2015/day-04/src/lib.rs
@@ -15,6 +15,7 @@ puzzle_info! {
 	part_two = |input| logic::part_two (& input);
 	commands = [
 		( name = "run"; method = cli::run; ),
+		( name = "find-test-case"; method = cli::find_test_case; ),
 	];
 }
 
@@ -266,6 +267,10 @@ mod cli {
 
 	use super::*;
 	use model::Input;
+	use std::io::Read as _;
+	use std::fs::File;
+	use std::sync::atomic::AtomicBool;
+	use std::sync::atomic::Ordering;
 
 	#[ derive (clap::Parser) ]
 	pub struct RunArgs {
@@ -298,6 +303,82 @@ mod cli {
 		Ok (())
 	}
 
+	#[ derive (Clone, clap::Parser) ]
+	pub struct FindTestCaseArgs {
+
+		#[ clap (long, default_value = "16") ]
+		len: usize,
+
+		#[ clap (long, default_value = "3999") ]
+		max: usize,
+
+	}
+
+	#[ allow (clippy::needless_collect) ]
+	#[ allow (clippy::needless_pass_by_value) ]
+	#[ allow (clippy::unnecessary_wraps) ]
+	pub fn find_test_case (args: FindTestCaseArgs) -> GenResult <()> {
+		assert! (args.len >= 4);
+		assert! (args.max < 0x_0100_0000);
+		let complete = Arc::new (AtomicBool::new (false));
+		let threads: Vec <_> = (0_u32 .. 4_u32)
+			.map (|_| {
+				let args = args.clone ();
+				let complete = Arc::clone (& complete);
+				std::thread::spawn (move || find_test_case_worker (args, complete))
+			})
+			.collect ();
+		threads.into_iter ().for_each (|thread| thread.join ().unwrap ());
+		Ok (())
+	}
+
+	#[ allow (clippy::needless_pass_by_value) ]
+	#[ allow (clippy::print_stdout) ]
+	#[ allow (clippy::string_slice) ]
+	fn find_test_case_worker (args: FindTestCaseArgs, complete: Arc <AtomicBool>) {
+		let mut rand = File::open ("/dev/urandom").unwrap ();
+		'OUTER: loop {
+			if complete.load (Ordering::Acquire) { break }
+			let num = loop {
+				let mut buf = [0; 3];
+				assert_eq! (3, rand.read (& mut buf).unwrap ());
+				let num =
+					(buf [0].as_usize () << 16_u32)
+						| (buf [1].as_usize () << 8_u32)
+						| buf [2].as_usize ();
+				if num <= args.max { break num }
+			};
+			let mut buffer = String::new ();
+			for _ in 0 .. args.len - 4 {
+				let ch = loop {
+					let mut buf = [0; 1];
+					assert_eq! (1, rand.read (& mut buf).unwrap ());
+					let ch = buf [0].as_char ();
+					if ('a' ..= 'z').contains (& ch) { break ch }
+				};
+				buffer.push (ch);
+			}
+			for chars in ('a' ..= 'z').permutations (4) {
+				buffer.truncate (args.len - 4);
+				for ch in chars { buffer.push (ch); }
+				write! (buffer, "{}", num).unwrap ();
+				let hash = md5::md5_hash (buffer.as_bytes ());
+				if hash [0] == 0 && hash [1] == 0 && hash [2] == 0 {
+					for num in 1 .. num {
+						let buffer = format! ("{}{}", & buffer [0 .. args.len], num);
+						let hash = md5::md5_hash (buffer.as_bytes ());
+						if hash [0] == 0 && hash [1] == 0 && hash [2] & 0xf0 == 0
+								&& hash [2] & 0xf != 0 {
+							println! ("{:?}", & buffer [0 .. args.len]);
+							complete.store (true, Ordering::Release);
+							break 'OUTER;
+						}
+					}
+				}
+			}
+		}
+	}
+
 }
 
 #[ cfg (test) ]
@@ -305,28 +386,20 @@ mod examples {
 
 	use super::*;
 
-	const EXAMPLE_ONE: & [& str] = & [
-		"NUM_ZEROS=1",
-		"MAX_THREADS=1",
-		"abcdef",
-	];
-
-	const EXAMPLE_TWO: & [& str] = & [
-		"NUM_ZEROS=2",
-		"MAX_THREADS=1",
-		"abcdef",
+	const EXAMPLE: & [& str] = & [
+		"xxebvswgx",
 	];
 
 	#[ test ]
 	fn part_one () {
 		let puzzle = puzzle_metadata ();
-		assert_eq_ok! ("31", puzzle.part_one (EXAMPLE_ONE));
+		assert_eq_ok! ("3206", puzzle.part_one (EXAMPLE));
 	}
 
 	#[ test ]
 	fn part_two () {
 		let puzzle = puzzle_metadata ();
-		assert_eq_ok! ("298", puzzle.part_two (EXAMPLE_TWO));
+		assert_eq_ok! ("4846", puzzle.part_two (EXAMPLE));
 	}
 
 }
