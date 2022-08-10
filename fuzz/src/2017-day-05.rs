cbb8f6f144cd326875388d97eaeb304dcb912a6d
James Pharaoh <james@pharaoh.uk>
2022-08-10 08:12:38 +0000

    2017/day-05: added

---
+++ fuzz/src/2017-day-05.rs
@@ -0,0 +1,12 @@
+#![ no_main ]
+
+use aoc_2017::day_05::*;
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target! (|input_str: & str| {
+	let input_vec: Vec <& str> = input_str.trim ().split ('\n').collect ();
+	if let Ok (input) = model::Input::parse (& input_vec) {
+		let _ = logic::part_one (& input);
+		let _ = logic::part_two (& input);
+	}
+});
