e256fbfb271e2dc26d7d4d5ddd7dc086c19a9110
James Pharaoh <james@pharaoh.uk>
2022-08-16 12:14:31 +0000

    2018/day-02, fuzz: refactor common fuzz mutator stuff into a macro

--- fuzz/src/2018-day-02.rs
+++ fuzz/src/2018-day-02.rs
@@ -1,9 +1,7 @@
 #![ no_main ]
 
-use libfuzzer_sys::fuzz_mutator;
+use aoc_fuzz::aoc_fuzz_mutator;
 use libfuzzer_sys::fuzz_target;
-use libfuzzer_sys::fuzzer_mutate;
-use rand::prelude::*;
 
 use aoc_common::*;
 use aoc_2018::day_02::*;
@@ -17,186 +15,89 @@ fuzz_target! (|input_str: & str| {
 	}
 });
 
+aoc_fuzz_mutator! {
 
-fuzz_mutator! (|data: & mut [u8], size: usize, max_size: usize, seed: u32| {
-	let mut rng = StdRng::seed_from_u64 (seed as u64);
-	let mut size = size;
-	let mut num_mutations = 0;
-	if rng.gen_bool (0.7) {
-		if let Some (new_size) = mutator::main (data, size, max_size, & mut rng) {
-			size = new_size;
-			num_mutations += 1;
+	transform_lifetimes = <'inp>;
+	input_type = Input <'inp>;
+
+	transform modify (100 * 1, 10 * 10, 1 * 100) = |input, rng| {
+		let idx = rng.gen_range (0 .. input.box_ids.len ());
+		let box_id = & mut input.box_ids [idx];
+		if box_id.is_empty () { return Some (()) }
+		let char_idx = rng.gen_range (0 .. box_id.chars ().count ());
+		let new_char = rng.gen_range ('a' ..= 'z');
+		input.box_ids [idx] = InpStr::alloc (
+			& box_id.chars ().enumerate ()
+				.map (|(idx, ch)| if idx == char_idx { new_char } else { ch })
+				.collect::<String> ());
+	}
+
+	transform add (100 * 1, 10 * 10, 1 * 100) = |input, rng| {
+		let len = input.box_ids.first ()
+			.map (|box_id| box_id.chars ().count ())
+			.unwrap_or (16);
+		let box_id = InpStr::alloc (
+			& iter::from_fn (|| Some (rng.gen_range ('a' ..= 'z')))
+				.take (len)
+				.collect::<String> ());
+		let new_idx = rng.gen_range (0 ..= input.box_ids.len ());
+		input.box_ids.insert (new_idx, box_id);
+	}
+
+	pub transform remove (100 * 1, 10 * 10, 1 * 100) = |input, rng| {
+		if input.box_ids.is_empty () { return Some (()) }
+		let idx = rng.gen_range (0 .. input.box_ids.len ());
+		input.box_ids.remove (idx);
+	}
+
+	transform add_char (10) = |input, rng| {
+		let len = input.box_ids.first ()
+			.map (|box_id| box_id.chars ().count ())
+			.unwrap_or (16);
+		let char_idx = rng.gen_range (0 ..= len);
+		let new_char = rng.gen_range ('a' ..= 'z');
+		for box_id in input.box_ids.iter_mut () {
+			* box_id = InpStr::alloc (
+				& iter::empty ()
+					.chain (box_id.chars ().take (char_idx))
+					.chain (iter::once (new_char))
+					.chain (box_id.chars ().skip (char_idx))
+					.collect::<String> ());
 		}
 	}
-	while num_mutations == 0 || rng.gen_bool (0.1) {
-		size = fuzzer_mutate (data, size, max_size);
-		num_mutations += 1;
-	}
-	size
-});
 
-mod mutator {
-
-	use super::*;
-
-	pub fn main (
-		data: & mut [u8],
-		size: usize,
-		max_size: usize,
-		rng: & mut StdRng,
-	) -> Option <usize> {
-
-		// parse input
-
-		let input_str = str::from_utf8 (& data [0 .. size]).ok () ?;
-		let input_vec: Vec <& str> = input_str.trim ().split ('\n').collect ();
-		let mut input = Input::parse (& input_vec).ok () ?;
-
-		// apply a random transform
-
-		transforms::random (& mut input, rng);
-
-		// convert to string, removing random lines until it is the right length
-
-		let output_str = loop {
-			let output_str = input.to_string ();
-			if output_str.as_bytes ().len () <= max_size { break output_str }
-			transforms::remove (& mut input, rng);
-		};
-
-		// update data, and return
-
-		(& mut data [ .. output_str.len ()]).copy_from_slice (output_str.as_bytes ());
-
-		Some (output_str.len ())
-
-	}
-
-	mod transforms {
-
-		use super::*;
-
-		type TransFn = for <'inp> fn (& mut Input <'inp>, & mut StdRng) -> Option <()>;
-
-		const TRANSFORMS: & [(u32, u32, TransFn)] = & [
-			(100, 1, modify),
-			(10, 10, modify),
-			(1, 100, modify),
-			(100, 1, add),
-			(10, 10, add),
-			(1, 100, add),
-			(100, 1, remove),
-			(10, 10, remove),
-			(1, 100, remove),
-			(10, 1, add_char),
-			(10, 1, remove_char),
-			(1, 1, truncate),
-			(1, 1, sort),
-			(1, 1, shuffle),
-		];
-
-		pub fn random <'inp> (input: & mut Input <'inp>, rng: & mut StdRng) {
-			let & (_, reps, ref trans_fn) =
-				TRANSFORMS.choose_weighted (rng, |& (weight, _, _)| weight).unwrap ();
-			let mut num_failure = 0;
-			for _ in 0 .. reps {
-				loop {
-					let success = trans_fn (input, rng).is_some ();
-					if success { break }
-					num_failure += 1;
-					if num_failure >= 10 { break }
-				}
-			}
-		}
-
-		fn modify <'inp> (input: & mut Input <'inp>, rng: & mut StdRng) -> Option <()> {
-			let idx = rng.gen_range (0 .. input.box_ids.len ());
-			let box_id = & mut input.box_ids [idx];
-			if box_id.is_empty () { return Some (()) }
-			let char_idx = rng.gen_range (0 .. box_id.chars ().count ());
-			let new_char = rng.gen_range ('a' ..= 'z');
-			input.box_ids [idx] = InpStr::alloc (
+	transform remove_char (10) = |input, rng| {
+		let len = input.box_ids.first ()
+			.map (|box_id| box_id.chars ().count ())
+			.unwrap_or (16);
+		let char_idx = rng.gen_range (0 ..= len);
+		for box_id in input.box_ids.iter_mut () {
+			* box_id = InpStr::alloc (
 				& box_id.chars ().enumerate ()
-					.map (|(idx, ch)| if idx == char_idx { new_char } else { ch })
+					.filter (|& (idx, _)| idx != char_idx)
+					.map (|(_, ch)| ch)
 					.collect::<String> ());
-			Some (())
 		}
+	}
 
-		fn add (input: & mut Input, rng: & mut StdRng) -> Option <()> {
-			let len = input.box_ids.first ()
-				.map (|box_id| box_id.chars ().count ())
-				.unwrap_or (16);
-			let box_id = InpStr::alloc (
-				& iter::from_fn (|| Some (rng.gen_range ('a' ..= 'z')))
-					.take (len)
-					.collect::<String> ());
-			let new_idx = rng.gen_range (0 ..= input.box_ids.len ());
-			input.box_ids.insert (new_idx, box_id);
-			Some (())
+	transform truncate (1) = |input, rng| {
+		let max_len = input.box_ids.iter ()
+			.map (|box_id| box_id.chars ().count ())
+			.max ()
+			.unwrap_or (0);
+		let len = rng.gen_range (0 ..= max_len);
+		for box_id in input.box_ids.iter_mut () {
+			if box_id.chars ().count () <= len { continue }
+			* box_id = InpStr::alloc (& box_id.chars ().take (len).collect::<String> ());
 		}
+	}
 
-		pub fn remove (input: & mut Input, rng: & mut StdRng) -> Option <()> {
-			if input.box_ids.is_empty () { return Some (()) }
-			let idx = rng.gen_range (0 .. input.box_ids.len ());
-			input.box_ids.remove (idx);
-			Some (())
-		}
+	transform sort (1) = |input, _rng| {
+		input.box_ids.sort ();
+	}
 
-		fn add_char (input: & mut Input, rng: & mut StdRng) -> Option <()> {
-			let len = input.box_ids.first ()
-				.map (|box_id| box_id.chars ().count ())
-				.unwrap_or (16);
-			let char_idx = rng.gen_range (0 ..= len);
-			let new_char = rng.gen_range ('a' ..= 'z');
-			for box_id in input.box_ids.iter_mut () {
-				* box_id = InpStr::alloc (
-					& iter::empty ()
-						.chain (box_id.chars ().take (char_idx))
-						.chain (iter::once (new_char))
-						.chain (box_id.chars ().skip (char_idx))
-						.collect::<String> ());
-			}
-			Some (())
-		}
-
-		fn remove_char (input: & mut Input, rng: & mut StdRng) -> Option <()> {
-			let len = input.box_ids.first ()
-				.map (|box_id| box_id.chars ().count ())
-				.unwrap_or (16);
-			let char_idx = rng.gen_range (0 ..= len);
-			for box_id in input.box_ids.iter_mut () {
-				* box_id = InpStr::alloc (
-					& box_id.chars ().enumerate ()
-						.filter (|& (idx, _)| idx != char_idx)
-						.map (|(_, ch)| ch)
-						.collect::<String> ());
-			}
-			Some (())
-		}
-
-		fn truncate <'inp> (input: & mut Input <'inp>, rng: & mut StdRng) -> Option <()> {
-			let max_len = input.box_ids.iter ()
-				.map (|box_id| box_id.chars ().count ())
-				.max ()
-				.unwrap_or (0);
-			let len = rng.gen_range (0 ..= max_len);
-			for box_id in input.box_ids.iter_mut () {
-				if box_id.chars ().count () <= len { continue }
-				* box_id = InpStr::alloc (& box_id.chars ().take (len).collect::<String> ());
-			}
-			Some (())
-		}
-
-		fn sort (input: & mut Input, _rng: & mut StdRng) -> Option <()> {
-			input.box_ids.sort ();
-			Some (())
-		}
-
-		fn shuffle (input: & mut Input, rng: & mut StdRng) -> Option <()> {
-			input.box_ids.shuffle (rng);
-			Some (())
-		}
-
+	transform shuffle (1) = |input, rng| {
+		input.box_ids.shuffle (rng);
 	}
 
 }
---
+++ fuzz/src/lib.rs
@@ -0,0 +1,126 @@
+#[ macro_export ]
+macro_rules! aoc_fuzz_mutator {
+	(
+		transform_lifetimes = <$trans_life:lifetime>;
+		input_type = $input_type:ty;
+		$(
+			$trans_vis:vis transform $trans_name:ident ($($trans_weights:tt)*) =
+			|$trans_input:ident, $trans_rng:ident| { $($trans_body:tt)* }
+		)*
+	) => {
+
+		::libfuzzer_sys::fuzz_mutator! (|
+			data: & mut [u8],
+			size: usize,
+			max_size: usize,
+			seed: u32,
+		| {
+			use ::rand::prelude::*;
+			let mut rng = StdRng::seed_from_u64 (seed as u64);
+			let mut size = size;
+			let mut num_mutations = 0;
+			if rng.gen_bool (0.7) {
+				if let Some (new_size) = mutator::main (data, size, max_size, & mut rng) {
+					size = new_size;
+					num_mutations += 1;
+				}
+			}
+			while num_mutations == 0 || rng.gen_bool (0.1) {
+				size = ::libfuzzer_sys::fuzzer_mutate (data, size, max_size);
+				num_mutations += 1;
+			}
+			size
+		});
+
+		mod mutator {
+
+			use super::*;
+			use ::rand::prelude::*;
+
+			pub fn main (
+				data: & mut [u8],
+				size: usize,
+				max_size: usize,
+				rng: & mut StdRng,
+			) -> Option <usize> {
+
+				// parse input
+
+				let input_str = str::from_utf8 (& data [0 .. size]).ok () ?;
+				let input_vec: Vec <& str> = input_str.trim ().split ('\n').collect ();
+				let mut input = Input::parse (& input_vec).ok () ?;
+
+				// apply a random transform
+
+				transforms::random (& mut input, rng);
+
+				// convert to string, removing random lines until it is the right length
+
+				let output_str = loop {
+					let output_str = input.to_string ();
+					if output_str.as_bytes ().len () <= max_size { break output_str }
+					transforms::remove (& mut input, rng);
+				};
+
+				// update data, and return
+
+				(& mut data [ .. output_str.len ()]).copy_from_slice (output_str.as_bytes ());
+
+				Some (output_str.len ())
+
+			}
+
+			mod transforms {
+
+				use super::*;
+
+				type TransFn = for <'inp> fn (& mut Input <'inp>, & mut StdRng) -> Option <()>;
+
+				aoc_fuzz_mutator! (@transforms $(( $trans_name $($trans_weights)* ))*);
+
+				pub fn random <'inp> (input: & mut Input <'inp>, rng: & mut StdRng) {
+					let & (_, reps, ref trans_fn) =
+						TRANSFORMS.choose_weighted (rng, |& (weight, _, _)| weight).unwrap ();
+					let mut num_failure = 0;
+					for _ in 0 .. reps {
+						loop {
+							let success = trans_fn (input, rng).is_some ();
+							if success { break }
+							num_failure += 1;
+							if num_failure >= 10 { break }
+						}
+					}
+				}
+
+				$(
+					$trans_vis fn $trans_name <$trans_life> (
+						$trans_input: & mut $input_type,
+						$trans_rng: & mut StdRng,
+					) -> Option <()> {
+						$($trans_body)*
+						Some (())
+					}
+				)*
+
+			}
+
+		}
+
+	};
+
+	(@transforms $($body:tt)*) => {
+		aoc_fuzz_mutator! (@transforms_impl [] $($body)*);
+	};
+	(@transforms_impl [$($data:tt)*] ($name:ident $weight:literal) $($rest:tt)*) => {
+		aoc_fuzz_mutator! (@transforms_impl [$($data)* ($weight, 1, $name)] $($rest)*);
+	};
+	(@transforms_impl [$($data:tt)*] ($name:ident $($weight:literal * $reps:literal),*) $($rest:tt)*) => {
+		aoc_fuzz_mutator! (@transforms_impl [$($data)* $(($weight, $reps, $name))*] $($rest)*);
+	};
+	(@transforms_impl [$(( $($data:tt)* ))*]) => {
+		const TRANSFORMS: & [(u32, u32, TransFn)] = & [
+			$(($($data)*)),*
+		];
+	};
+
+}
